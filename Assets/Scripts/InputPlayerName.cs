ae21b6a063172e715091c975a13365deb9a9113d
Paula-LL <plizelucea@gmail.com>
2025-11-13 19:29:26 +0100

    Input name Script changes

--- Assets/Scripts/InputPlayerName.cs
+++ Assets/Scripts/InputPlayerName.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputPlayerName : MonoBehaviour
 {
-    public TMP_Text userName;
+    string userName;
+    public Button button;
+    public TMP_InputField field;
+
+    private void Start()
+    {
+        button.enabled = !string.IsNullOrEmpty(userName);
+        Debug.Log(string.IsNullOrEmpty(userName));
+    }
+
     public void SetUserName(string text)
     {
-        userName.text = text.ToString(); 
+        userName = text.ToString();
+        button.enabled = !string.IsNullOrEmpty(userName);
     }
+
+    private void Awake()
+    {
+        field.onEndEdit.AddListener(SetUserName);
+    }
+
 }
