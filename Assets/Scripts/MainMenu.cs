eeefbd747f8c88668ac993bffbc3eb9335270994
Paula-LL <plizelucea@gmail.com>
2025-11-13 17:24:02 +0100

    Start Settings Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   /* public void InputNameToPlay() {
-        SceneManager.LoadSceneAsync("");
-    }*/
+   public void InputNameToPlay() {
+        SceneManager.LoadSceneAsync("KahootSelector");
+    }
 }
